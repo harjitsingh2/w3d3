0bafc343198cc409b7a1dc8fb055575a02c89fca
Tyvan Cheng <tyvancheng@gmail.com>
2023-03-22 15:12:52 -0400

    added fib

--- recursion.rb
+++ recursion.rb
@@ -36,3 +36,33 @@ end
 # p exp2(2,4) # 16
 # p exp2(3,3) # 27
 
+def deep_dup(arr)
+    new_arr = []
+    
+    return arr if arr.none? {|el| el.is_a?Array}
+
+    arr.each do |el|
+        if !el.is_a?Array
+            new_arr << el
+        else
+            new_arr << deep_dup(el)
+        end
+    end
+    new_arr
+end
+
+# p deep_dup([1,[2],[3,4]])
+
+def fib(n)
+    return [] if n == 0
+    return [0] if n == 1
+    return [1] if n == 2
+
+    fib(n-1) << fib(n-1)[-1] + fib(n-2)[-1]
+
+end
+
+# p fib(5)
+
+def
+
