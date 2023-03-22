9d481f54178d13e8aeb6d2386f1afd6659dba7f5
harjitsingh3 <singh.harjit09@gmail.com>
2023-03-22 14:36:52 -0400

    add exponent methods

--- recursion.rb
+++ recursion.rb
@@ -11,3 +11,28 @@ end
 
 # p range(1,5)
 
+
+def exp1(b,n)
+    return 1 if n==0
+    b * exp1(b, n-1)
+end
+
+# p exp1(2,3) # 8
+# p exp1(2,4) # 16
+# p exp1(3,3) # 27
+
+def exp2(b,n)
+    return 1 if n == 0
+    return b if n == 1
+   
+    if n % 2 == 0
+        exp2(b, n/2) * exp2(b, n/2)
+    else 
+        b * (exp2(b, (n-1)/2) * exp2(b, (n-1)/2))
+    end 
+end 
+
+# p exp2(2,3) # 8
+# p exp2(2,4) # 16
+# p exp2(3,3) # 27
+
